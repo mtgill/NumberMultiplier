64d21d513355c95bd5c818882b64ccb55910cefe
Matt Gill <mtgill90@outlook.com>
2019-08-21 19:17:08 -0500

    first commit, numbers do the right stuff

---
+++ NumberMultiplier/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberMultiplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a list of comma seperated numbers");
+
+            var numInputs = Console.ReadLine();
+
+            var numList = numInputs.Split(",");
+
+            List<int> myNums = new List<int>();
+
+            for (var i = 0; i < numList.Length; i++)
+            {
+                myNums.Add(int.Parse(numList[i]));
+            }
+
+            Console.WriteLine("Would you like to multiply them together or square each value?");
+            var mathType = Console.ReadLine();
+            mathType.ToLower();
+
+            if (mathType == "multiply")
+            {
+                int currentVal = 1;
+
+                for (var i = 0; i < myNums.Count; i++)
+                {
+                    currentVal = currentVal * myNums[i];
+                }
+
+                Console.Write(currentVal.ToString());
+                return;
+            } else if (mathType == "square")
+            {
+                int sqrdVal;
+
+                for (var i = 0; i < myNums.Count; i++)
+                {
+                    var currentVal = myNums[i];
+                    sqrdVal = currentVal * currentVal;
+                    sqrdVal.ToString();
+                    Console.Write($"{sqrdVal},");
+                }
+            } else
+            {
+                Console.WriteLine("Better luck next time!");
+            }
+
+        }
+    }
+}
